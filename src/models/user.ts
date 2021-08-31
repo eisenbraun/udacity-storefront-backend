6031ec177428cc1cece0d2b3438003d2392119fa
Michael Eisenbraun <eisenbm@algonquincollege.com>
2021-08-31 18:53:04 -0400

    Creates user model with tests

---
+++ src/models/user.ts
@@ -0,0 +1,53 @@
+import db from '../database' // @ts-ignore
+
+export type User = {
+  id: number | string;
+  first_name: string;
+  last_name: string;
+  password: string;
+}
+
+export class UserStore {
+  async index(): Promise<User[]> {
+    try {
+      const conn = await db.connect()
+      const sql = 'SELECT * FROM users;'
+      const result = await conn.query(sql)
+      const users = result.rows
+      conn.release()
+      
+      return users
+    } catch (err) {
+      throw new Error(`Could not get users. Error ${err}`)
+    }
+  } 
+
+  async show(id: number | string): Promise<User> {
+    try {
+      const conn = await db.connect()
+      const sql = 'SELECT * FROM users WHERE id=($1);'
+      const result = await conn.query(sql, [id])
+      const user = result.rows[0]
+      conn.release()
+      
+      return user
+    } catch (err) {
+      throw new Error(`Could not find user. Error ${err}`)
+    }
+  } 
+
+  async create(u: User): Promise<User> {
+    try {
+      const conn = await db.connect()
+      const sql = 'INSERT INTO users (first_name, last_name, password) VALUES ($1, $2, $3) RETURNING *;'
+      const result = await conn.query(sql, [u.first_name, u.last_name, u.password])
+      const user = result.rows[0]
+
+      conn.release()
+      
+      return user
+    } catch (err) {
+      throw new Error(`Could not add a new user. Error ${err}`)
+    }
+  } 
+}
