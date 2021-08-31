3d0065f37fc02133e7a73c28125eab68a24289e5
Michael Eisenbraun <eisenbm@algonquincollege.com>
2021-08-30 21:57:19 -0400

    Sets up database connection

--- src/server.ts
+++ src/server.ts
@@ -1,13 +1,19 @@
 import express, { Request, Response } from 'express'
 import bodyParser from 'body-parser'
+import db from './database' // @ts-ignore
 
 const app: express.Application = express()
 const address: string = "0.0.0.0:3000"
 
 app.use(bodyParser.json())
 
-app.get('/', function (req: Request, res: Response) {
-    res.send('Hello World!')
+app.get('/', async function (req: Request, res: Response) {
+    try {
+        const conn = await db.connect()
+        res.send('DB connected')
+    } catch (err) {
+        res.send(`Error: ${err}`)
+    }
 })
 
 app.listen(3000, function () {
